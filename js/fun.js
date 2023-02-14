d5c0d4ce99a2bc432c3cb983f31555dc342aa90a
abdullah-qdad <qdad.2000@gmail.com>
2023-02-14 15:21:53 +0000

    fillter funcation

--- js/fun.js
+++ js/fun.js
@@ -3,71 +3,70 @@
 var isLoggedIn = false;
 
 const flowers = [
-    { name: "Rose", description: "A beautiful red flower.", price: 2.99, number: 20 },
-    { name: "Lily", description: "A tall, elegant white flower.", price: 3.99, number: 15 },
-    { name: "Sunflower", description: "A large, yellow flower that follows the sun.", price: 1.99, number: 25 },
-    { name: "Tulip", description: "A brightly colored spring flower.", price: 2.49, number: 30 },
-    { name: "Daffodil", description: "A cheerful yellow spring flower.", price: 1.49, number: 35 },
-    { name: "Orchid", description: "A exotic, colorful flower.", price: 4.99, number: 10 },
-    { name: "Iris", description: "A tall, colorful spring flower.", price: 3.49, number: 20 },
-    { name: "Daisy", description: "A cheerful white and yellow flower.", price: 0.99, number: 40 },
-    { name: "Peony", description: "A large, lush flower in shades of pink, red, and white.", price: 5.99, number: 5 },
-    { name: "Lilac", description: "A fragrant, purple spring flower.", price: 2.99, number: 15 }
+  { name: "Rose", description: "A beautiful red flower.", price: 2.99, number: 20, occasion: "VD" },
+  { name: "Lily", description: "A tall, elegant white flower.", price: 3.99, number: 15, occasion: "VD" },
+  { name: "Sunflower", description: "A large, yellow flower that follows the sun.", price: 1.99, number: 25, occasion: "BD" },
+  { name: "Tulip", description: "A brightly colored spring flower.", price: 2.49, number: 30, occasion: "BD" },
+  { name: "Daffodil", description: "A cheerful yellow spring flower.", price: 1.49, number: 35, occasion: "NY" },
+  { name: "Orchid", description: "A exotic, colorful flower.", price: 4.99, number: 10, occasion: "NY" },
+  { name: "Iris", description: "A tall, colorful spring flower.", price: 3.49, number: 20, occasion: "NB" },
+  { name: "Daisy", description: "A cheerful white and yellow flower.", price: 0.99, number: 40, occasion: "NB" },
+  { name: "Peony", description: "A large, lush flower in shades of pink, red, and white.", price: 5.99, number: 5, occasion: "S" },
+  { name: "Lilac", description: "A fragrant, purple spring flower.", price: 2.99, number: 15, occasion: "S" }
 ];
 
 display(flowers);
 
 function display(array) {
-    const tbody = document.getElementById('flowerData');
+  const tbody = document.getElementById('flowerData');
+  tbody.innerHTML = '';
+  for (let i = 0; i < array.length; i++) {
+    const flower = array[i];
 
-    for (let i = 0; i < array.length; i++) {
-        const flower = array[i];
+    const row = document.createElement("tr");
+    tbody.appendChild(row);
 
-        const row = document.createElement("tr");
-        tbody.appendChild(row);
+    const nameCell = document.createElement("td");
+    nameCell.innerHTML = flower.name;
+    row.appendChild(nameCell);
 
-        const nameCell = document.createElement("td");
-        nameCell.innerHTML = flower.name;
-        row.appendChild(nameCell);
+    const descriptionCell = document.createElement("td");
+    descriptionCell.innerHTML = flower.description;
+    row.appendChild(descriptionCell);
 
-        const descriptionCell = document.createElement("td");
-        descriptionCell.innerHTML = flower.description;
-        row.appendChild(descriptionCell);
+    const priceCell = document.createElement("td");
+    priceCell.innerHTML = "$" + flower.price;
+    row.appendChild(priceCell);
 
-        const priceCell = document.createElement("td");
-        priceCell.innerHTML = "$" + flower.price;
-        row.appendChild(priceCell);
+    const numberCell = document.createElement("td");
+    numberCell.innerHTML = flower.number;
+    row.appendChild(numberCell);
 
-        const numberCell = document.createElement("td");
-        numberCell.innerHTML = flower.number;
-        row.appendChild(numberCell);
+    const buttonCell = document.createElement("td");
+    const button = document.createElement("button");
+    button.innerText = "Buy";
+    button.addEventListener("click", function () {
+      if (!isLoggedIn) {
+        alert("Please Login First");
+      } else {
+        if (flower.number > 0) {
+          flower.number--;
+          numberCell.innerHTML = flower.number;
+        } else {
+          alert("Out of stock");
+        }
+      }
 
-        const buttonCell = document.createElement("td");
-        const button = document.createElement("button");
-        button.innerText = "Buy";
-        // button.id = `buyButton${i}`;
-        button.addEventListener("click", function () {
-          if (!isLoggedIn) {
-            alert("Please Login First");
-          } else {
-            if (flower.number > 0) {
-              flower.number--;
-              numberCell.innerHTML = flower.number;
-          } else {
-              alert("Out od stock");
-          }
-          }
-            
 
-        });
-        buttonCell.appendChild(button);
-        row.appendChild(buttonCell);
+    });
+    buttonCell.appendChild(button);
+    row.appendChild(buttonCell);
 
-    }
+  }
 }
 
 const form = document.getElementById("loginForm");
-form.addEventListener("submit", function(event) {
+form.addEventListener("submit", function (event) {
   event.preventDefault();
 
   const email = form.elements.email.value;
@@ -85,8 +84,21 @@ function validateEmail(email) {
 }
 
 function userName(name) {
-    document.getElementById("username").textContent = name;
+  document.getElementById("username").textContent = name;
 }
 
-// const buyButtons = document.querySelectorAll("button[id^='buyButton']");
-// console.log(buyButtons);
+
+const links = document.querySelectorAll('.occasion');
+links.forEach(link => {
+  link.addEventListener('click', (event) => {
+    const value = link.getAttribute('value');
+    const newArray = [];
+    let length = flowers.length;
+    for (let i = 0; i < length; i++) {
+      if (flowers[i].occasion == value) {
+        newArray.push(flowers[i]);
+      }
+    }
+    display(newArray);
+  });
+});
